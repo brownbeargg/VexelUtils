2f6d0cdd86caa64c791aa13ecf3b8646f20a0ec2
binarybear <mikailoudem@gmail.com>
2026-05-13 19:54:02 +0200

    Added main structure of util library

---
+++ source/Memory/RefCount.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "ClawSTL.hpp"
+
+namespace Claw
+{
+    struct MemoryControlBlock
+    {
+        uint32_t RefCount = 0;
+        uint32_t WeakCount = 0;
+    };
+
+    struct RefCount
+    {
+        RefCount() = default;
+        virtual ~RefCount() = default;
+
+        RefCount(const RefCount& other) = delete;
+        RefCount(RefCount&& rhs) noexcept = delete;
+        RefCount& operator=(const RefCount& other) = delete;
+        RefCount& operator=(RefCount&& rhs) noexcept = delete;
+
+        uint32_t GetRefCount() { return m_ControlBlock.RefCount; }
+        uint32_t GetWeakCount() { return m_ControlBlock.WeakCount; }
+
+        uint32_t GetTotalPtrCount() { return m_ControlBlock.RefCount + m_ControlBlock.WeakCount; }
+
+      private:
+        void IncRefCount() { ++m_ControlBlock.RefCount; }
+        void DecRefCount() { --m_ControlBlock.RefCount; }
+
+        void IncWeakCount() { ++m_ControlBlock.WeakCount; }
+        void DecWeakCount() { --m_ControlBlock.WeakCount; }
+
+      private:
+        mutable MemoryControlBlock m_ControlBlock{};
+
+        friend class Ref;
+        friend class Weak;
+    };
+} // namespace Claw
