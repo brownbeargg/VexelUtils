e893e18a89acae3f2d2795554a902a5b63998dd6
binarybear <mikailoudem@gmail.com>
2026-05-15 18:05:02 +0200

    added more STL headers

--- include/Vexel/STL.hpp
+++ include/Vexel/STL.hpp
@@ -1,6 +1,17 @@
+/* STREAMS */
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <streambuf>
+#include <syncstream>   // C++20
+
 /* CONTAINERS */
 #include <array>
-#include <bitset>
 #include <deque>
 #include <forward_list>
 #include <list>
@@ -12,36 +23,65 @@
 #include <unordered_set>
 #include <vector>
 
-/* THREADING HEADERS */
+/* STRINGS */
+#include <charconv>
+#include <format>       // C++20
+#include <regex>
+#include <string>
+#include <string_view>
+
+/* ALGORITHM | ITERATORS */
+#include <algorithm>
+#include <execution>
+#include <iterator>
+#include <ranges>       // C++20
+
+/* THREADS */
 #include <atomic>
+#include <barrier>      // C++20
 #include <condition_variable>
 #include <future>
+#include <latch>        // C++20
 #include <mutex>
+#include <semaphore>    // C++20
+#include <shared_mutex>
+#include <stop_token>   // C++20
 #include <thread>
 
-/* MISC */
-#include <algorithm>
-#include <chrono>
-#include <codecvt>
+/* NUMBERS | MATH | BITWISE */
+#include <bit>          // C++20
+#include <bitset>
 #include <complex>
-#include <exception>
-#include <functional>
-#include <initializer_list>
-#include <iterator>
 #include <limits>
-#include <locale>
-#include <memory>
-#include <new>
+#include <numbers>      // C++20
 #include <numeric>
 #include <random>
-#include <ratio>
-#include <regex>
+#include <valarray>
+
+/* SYSTEM | MEMORY | UTILITY */
+#include <any>
+#include <chrono>
+#include <compare>      // C++20
+#include <concepts>     // C++20
+#include <coroutine>    // C++20
+#include <exception>
+#include <filesystem>
+#include <functional>
+#include <initializer_list>
+#include <locale>
+#include <memory>
+#include <memory_resource>
+#include <new>
+#include <optional>
+#include <scoped_allocator>
+#include <source_location> // C++20
+#include <span>         // C++20
 #include <stdexcept>
-#include <string>
 #include <system_error>
 #include <tuple>
-#include <type_traits>
 #include <typeindex>
 #include <typeinfo>
+#include <type_traits>
 #include <utility>
-#include <valarray>
+#include <variant>
+#include <version>
