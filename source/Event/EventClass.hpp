c8184cf9016d91e2a0329d093e7cf904b66b7f73
binarybear <mikailoudem@gmail.com>
2026-05-20 20:46:37 +0200

    added new event options

--- source/Event/EventClass.hpp
+++ source/Event/EventClass.hpp
@@ -10,7 +10,21 @@ namespace Vex
 
         AppTick = VEX_BIT(1),
         AppUpdate = VEX_BIT(2),
-        AppRender = VEX_BIT(3)
+        AppRender = VEX_BIT(3),
+
+        WindowClose = VEX_BIT(4),
+        WindowResize = VEX_BIT(5),
+        WindowFocus = VEX_BIT(6),
+        WindowLostFocus = VEX_BIT(7),
+        WindowMoved = VEX_BIT(8),
+
+        KeyPressed = VEX_BIT(9),
+        KeyReleased = VEX_BIT(10),
+
+        MouseButtonPressed = VEX_BIT(11),
+        MouseButtonReleased = VEX_BIT(12),
+        MouseMoved = VEX_BIT(13),
+        MouseScrolle = VEX_BIT(14)
     };
 
     enum class EventCategory
@@ -18,6 +32,9 @@ namespace Vex
         None = 0,
 
         App = VEX_BIT(1),
+        Input = VEX_BIT(2),
+        Keyboard = VEX_BIT(3),
+        Mouse = VEX_BIT(4),
     };
 
     class Event : public RefCount
